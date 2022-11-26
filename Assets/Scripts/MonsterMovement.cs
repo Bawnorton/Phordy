538f58f715f37b7df2585eb771ad187247ad7292
chpsmstr <prakashankkit@gmail.coma>
2022-11-26 12:55:52 -0800

    new-monster branch chases player, does not collide with any objects beside player only moves in X axis

--- Assets/Scripts/MonsterMovement.cs
+++ Assets/Scripts/MonsterMovement.cs
@@ -9,29 +9,17 @@ public class MonsterMovement : MonoBehaviour
 
     // private GameObject floor;
     private Rigidbody rb;
-    private GameObject Player;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        // floor = GameObject.Find("Floor");
-        GameObject Player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
         var horizontal = 1;
-        var vertical = 0;
         var velocity = rb.velocity;
 
-        rb.velocity = new Vector3(horizontal * SPEED, velocity.y, vertical * SPEED);
-        //if (Input.GetButtonDown("Jump")) rb.velocity = new Vector3(velocity.x, SPEED, velocity.z);
-
-        var playerZ = Player.transform.position.z;
-        var monsterPos = rb.transform.position;
-        rb.transform.position = new Vector3(monsterPos.x,monsterPos.y,playerZ);
-
-        // var floorPos = floor.transform.position;
-        // floor.transform.position = new Vector3(floorPos.x, floorPos.y, rb.position.z);
+        rb.velocity = new Vector3(horizontal * SPEED, velocity.y, 0);
     }
 }
