b4f40cfeeeeda62275987b32fb924a69a71b2b43
Benjamin-Norton <bawnorton@me.com>
2022-11-26 11:52:55 -0800

    Save to fix ankkit f up :)

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,6 +6,8 @@ using Random = UnityEngine.Random;
 public class GameController : MonoBehaviour {
     
     public static GameObject floor;
+    public const int MAX_Z = 5;
+    public const int MIN_Z = -5;
     private void Start() {
         floor = GameObject.Find("Floor");
     }
