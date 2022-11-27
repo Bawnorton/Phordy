b1a450e2236e3e795a45dd79548d612305bd334a
Jayden J <jaydenjaya@gmail.com>
2022-11-26 21:36:36 -0800

    added music and set main menu volume slider

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Start()
+    {
+        
+    }
+}
